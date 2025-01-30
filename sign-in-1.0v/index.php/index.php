dd03b8cd0e5bc3f076bb6f1dfda9b4e0cd6b5083
kushal <kushal@gmail.com>
2025-01-30 19:44:45 +0530

    modified changes

--- sign-in-1.0v/index.php/index.php
+++ sign-in-1.0v/index.php/index.php
@@ -3,9 +3,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PHP - Hello, World!</title>
+    <title>PHP - login user!</title>
 </head>
 <body>
-        <h1><?php echo 'Hello, World!'; ?></h1>
+        <h1><?php echo 'user login!'; ?></h1>
 </body>
 </html>
